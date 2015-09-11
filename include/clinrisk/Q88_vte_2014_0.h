54fd5e3edf90e3e4c844515d94763a0e428b3e1c
s.d.hippisley-cox <s.d.hippisley-cox@762b0a03-bbb0-4652-8c00-258f7baf3196>
2015-09-11 09:43:23 +0000

    Release of QThrombosis-2014

---
+++ include/clinrisk/Q88_vte_2014_0.h
@@ -0,0 +1,34 @@
+/* 
+ * Copyright 2014 ClinRisk Ltd.
+ * 
+ * This file is part of QThrombosis-2014 (http://qthrombosis.org, http://svn.clinrisk.co.uk/opensource/qthrombosis).
+ * 
+ * QThrombosis-2014 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * QThrombosis-2014 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with QThrombosis-2014.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * The initial version of this file, to be found at http://svn.clinrisk.co.uk/opensource/qthrombosis, faithfully implements QThrombosis-2014.
+ * We have released this code under the GNU Affero General Public License to enable others to implement the algorithm faithfully.
+ * However, the nature of the GNU Affero General Public License is such that we cannot prevent, for example, someone accidentally 
+ * altering the coefficients, getting the inputs wrong, or just poor programming.
+ * We stress, therefore, that it is the responsibility of the end user to check that the source that they receive produces the same results as the original code posted at http://svn.clinrisk.co.uk/opensource/qthrombosis.
+ * Inaccurate implementations of risk scores can lead to wrong patients being given the wrong treatment.
+ * 
+ * This file has been auto-generated.
+ * XML source: Q88_vte_2014_0.xml
+ * STATA dta time stamp: 17 Jun 2014 22:53
+ * Header file create date: Fri 11 Sep 2015 10:36:27 BST
+ */
+
+double vte_female(
+int age,int b_CCF,int b_admit,int b_antipsychotic,int b_anycancer,int b_cop,int b_copd,int b_hrt,int b_ibd,int b_renal,int b_tamoxifen,int b_varicosevein,double bmi,int smoke_cat,int surv,double town,int *error,char *errorBuf,int errorBufSize
+);
